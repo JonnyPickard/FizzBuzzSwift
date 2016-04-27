72f27510bb23b1f5db2a0803842c86e4f9b2832b
Jonny Pickard <jonathandpickard@gmail.com>
2016-04-27 21:51:42 +0100

    Initial commit, Xcode setup, first test written

---
+++ Brain.swift
@@ -0,0 +1,17 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Jonny Pickard on 27/04/2016.
+//  Copyright © 2016 Jonny. All rights reserved.
+//
+
+import Foundation
+
+class Brain: NSObject {
+    
+    func isDivisibleByThree(number :Int) -> Bool {
+        return true
+    }
+    
+}
